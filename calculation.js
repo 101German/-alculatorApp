b5b46fc26a4a4d9b63bd81f64c058d1198673386
German Shvayukou <geras210e@gmail.com>
2023-01-11 21:08:09 +0300

    implement command pattern

--- calculation.js
+++ calculation.js
@@ -1,15 +1,50 @@
+let ctrl = false;
+let z = false;
 let leftOperand = '';
 let rightOperand = '';
 let sign = '';
 let isCalculated = false;
 let savedNum = '';
-
 let digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 let signs = ['mc', 'm+', 'm-', 'mr', '+/-', '%', '/', 'x^2', 'x^3', 'x^y', '10^x', 'x', '1/x', '2√x', '3√x', 'y√x', '-', 'x!', '+'];
+
+let invoker = new Invoker();
 
 var calcScreen = document.querySelector('.calc-screen p');
 
 document.querySelector('.ac').onclick = clearAll;
+document.addEventListener('keyup', (event) => {
+    if (event.keyCode === 17) {
+        console.log('up ctrl');
+        ctrl = false;
+    }
+    else if (event.keyCode === 90) {
+        console.log('up z');
+        z = false;
+    }
+})
+
+document.addEventListener('keydown', (event) => {
+    if (event.keyCode === 17) {
+        ctrl = true;
+    }
+    else if (event.keyCode === 90) {
+        z = true;
+    }
+
+    if (z && ctrl) {
+        var res = invoker.cancel();
+        console.log('res ' + res)
+        if (res != undefined) {
+            console.log('und')
+            sign = '';
+            leftOperand = res;
+            calcScreen.textContent = leftOperand;
+        }
+        z = false;
+        ctrl = false;
+    }
+})
 
 document.querySelector('.buttons').onclick = (event) => {
     if (!event.target.classList.contains('btn')) return;
@@ -61,29 +96,39 @@ document.querySelector('.buttons').onclick = (event) => {
     }
 
     if (signs.includes(key)) {
+        console.log('here')
         if (key === 'mc') {
-            savedNum = '';
+            var command = new MemoryClearCommand();
+            invoker.setCommand(command);
+            invoker.run();
         }
-        else if (key === 'mr' && savedNum != '') {
-            calcScreen.textContent = savedNum;
-            leftOperand = savedNum;
+        else if (key === 'mr' && savedNum !== '') {
+            var command = new MemoryReadCommand();
+            invoker.setCommand(command);
+            invoker.run();
         }
         else if (key === 'm+') {
-            savedNum = savedNum === '' ? calcScreen.textContent : (+savedNum) + (+calcScreen.textContent);
-            leftOperand = '';
+            var command = new MemoryAddCommand();
+            invoker.setCommand(command);
+            invoker.run();
         }
         else if (key === 'm-') {
-            savedNum = savedNum === '' ? -calcScreen.textContent : savedNum - calcScreen.textContent;
-            leftOperand = '';
+            var command = new MemorySubtractionCommand();
+            invoker.setCommand(command);
+            invoker.run();
         }
-        else if ((sign != '' && rightOperand != '') || key === '+/-' || key === '%' || key === 'x^2' || key === 'x^3'
-            || key === 'x^y' || key === '10^x' || key === '1/x' || key === '2√x' || key === '3√x' || key === 'y√x'
+        else if (sign != '' && rightOperand != '') {
+            calculate();
+            sign = key;
+        }
+        else if (key === '+/-' || key === '%' || key === 'x^2' || key === 'x^3'
+            || (leftOperand != '' && rightOperand != '' && key === 'x^y') || key === '10^x' || key === '1/x' || key === '2√x' || key === '3√x' || key === 'y√x'
             || key === 'x!') {
             sign = key;
             console.log(sign);
             calculate()
         }
-        else{
+        else {
             sign = key;
         }
 
@@ -96,55 +141,65 @@ document.querySelector('.buttons').onclick = (event) => {
 }
 
 function calculate() {
-
     isCalculated = true;
+    var command;
     switch (sign) {
         case '+':
-            leftOperand = leftOperand == '' ? rightOperand : (+leftOperand) + (+rightOperand);
+            command = new AdditionCommand(leftOperand, rightOperand);
+            invoker.setCommand(command);
+            leftOperand = invoker.run();
             break;
         case '-':
-            leftOperand = leftOperand == '' ? -rightOperand : leftOperand - rightOperand;
+            command = new SubtractionCommand(leftOperand, rightOperand);
+            invoker.setCommand(command);
+            leftOperand = invoker.run();
             break;
         case 'x':
-            leftOperand = leftOperand == '' ? '0' : leftOperand * rightOperand;
+            command = new MultiplicationCommand(leftOperand, rightOperand);
+            invoker.setCommand(command);
+            leftOperand = invoker.run();
             break;
         case '/':
-            if (rightOperand === '0') {
-                clearAll();
-                calcScreen.textContent = 'Error';
-                throw new Error("division on zero");
-            }
-            else {
-                leftOperand = leftOperand == '' ? '0' : leftOperand / rightOperand;
-            }
+            command = new DivisionCommand(leftOperand, rightOperand);
+            invoker.setCommand(command);
+            leftOperand = invoker.run();
             break;
         case 'x^2':
             if (rightOperand != '') {
-                leftOperand = computePower(rightOperand, 2);
+                command = new ComputePowerCommand(rightOperand, 2);
             }
             else {
-                leftOperand = computePower(leftOperand, 2);
+                command = new ComputePowerCommand(leftOperand, 2);
             }
+            invoker.setCommand(command);
+            leftOperand = invoker.run();
             break;
         case 'x^3':
             if (rightOperand != '') {
-                leftOperand = computePower(rightOperand, 3);
+                command = new ComputePowerCommand(rightOperand, 3);
             }
             else {
-                leftOperand = computePower(leftOperand, 3);
+                command = new ComputePowerCommand(leftOperand, 3);
             }
+            invoker.setCommand(command);
+            leftOperand = invoker.run();
             break;
         case '10^x':
+            console.log('10')
             if (rightOperand != '') {
-                leftOperand = computePower(10, rightOperand);
+                command = new ComputePowerCommand(10, rightOperand);
             }
             else {
-                leftOperand = computePower(10, leftOperand);
+                command = new ComputePowerCommand(10, leftOperand);
             }
+            invoker.setCommand(command);
+            leftOperand = invoker.run();
             break;
         case 'x^y':
             if (leftOperand != '' && rightOperand != '') {
-                leftOperand = computePower(leftOperand, rightOperand);
+                command = new ComputePowerCommand(leftOperand, rightOperand);
+                invoker.setCommand(command);
+                leftOperand = invoker.run();
             }
             else {
                 return;
@@ -153,75 +208,74 @@ function calculate() {
         case '1/x':
             console.log('1/x log')
             if (rightOperand != '') {
-                if (rightOperand === '0') {
-                    clearAll();
-                    calcScreen.textContent = 'Error';
-                    throw new Error("division on zero");
-                }
-                else {
-                    leftOperand = '1' / rightOperand;
-                }
+                command = new DivisionCommand(1, rightOperand);
             }
             else {
-                if (leftOperand === '0' || leftOperand === '') {
-                    clearAll();
-                    calcScreen.textContent = 'Error';
-                    throw new Error("division on zero");
-                }
-                else {
-                    leftOperand = '1' / leftOperand;
-                }
+                command = new DivisionCommand(1, leftOperand);
             }
+            invoker.setCommand(command);
+            leftOperand = invoker.run();
             break;
         case '%':
             if (rightOperand != '') {
-                leftOperand = rightOperand / '100';
+                command = new DivisionCommand(rightOperand, 100);
             }
             else {
-                leftOperand = leftOperand / '100';
+                command = new DivisionCommand(leftOperand, 100);
             }
+
+            invoker.setCommand(command);
+            leftOperand = invoker.run()
             break;
         case '2√x':
             if (rightOperand != '') {
-                leftOperand = computePower(leftOperand, 0.5);
+                command = new ComputePowerCommand(rightOperand, 0.5);
             }
             else {
-                leftOperand = computePower(leftOperand, 0.5);
+                command = new ComputePowerCommand(leftOperand, 0.5);
             }
+            invoker.setCommand(command);
+            leftOperand = invoker.run();
             break;
         case '3√x':
             if (rightOperand != '') {
-                leftOperand = computePower(leftOperand, 1 / 3);
+                command = new ComputePowerCommand(rightOperand, 1 / 3);
             }
             else {
-                leftOperand = computePower(leftOperand, 1 / 3);
+                command = new ComputePowerCommand(leftOperand, 1 / 3);
             }
+            invoker.setCommand(command);
+            leftOperand = invoker.run();
             break;
         case 'y√x':
             if (rightOperand === '') {
-                leftOperand = computePower(leftOperand, 1 / leftOperand);
+                command = new ComputePowerCommand(leftOperand, 1 / leftOperand);
             }
             else {
-                console.log('try')
-                leftOperand = computePower(leftOperand, 1 / rightOperand);
-                console.log('left operand after calculate:' + leftOperand);
+                command = new ComputePowerCommand(leftOperand, 1 / rightOperand);
             }
+            invoker.setCommand(command);
+            leftOperand = invoker.run();
             break;
         case 'x!':
             if (rightOperand != '') {
-                leftOperand = factorial(rightOperand);
+                command = new FactorialCommand(rightOperand);
             }
             else {
-                leftOperand = factorial(leftOperand);
+                command = new FactorialCommand(leftOperand);
             }
+            invoker.setCommand(command);
+            leftOperand = invoker.run();
             break;
         case '+/-':
             if (rightOperand != '') {
-                leftOperand = rightOperand * (-1);
+                command = new MultiplicationCommand(rightOperand, -1);
             }
             else {
-                leftOperand = leftOperand * (-1);
+                command = new MultiplicationCommand(leftOperand, -1);
             }
+            invoker.setCommand(command);
+            leftOperand = invoker.run();
             break;
         default:
             isCalculated = false;
